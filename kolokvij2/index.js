139b3f8ac5749626faef85a48ed6d87451c0c40a
magdalenajurkic <magdalena.jurkic2002@gmail.com>
2025-12-22 13:28:21 +0100

    kolokvij

---
+++ kolokvij2/index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    let broj = prompt("Unesite jedan broj:");
+    broj = Number(broj);
+    let rezultat = broj * 2;
+
+    alert("Unio si broj " + broj + ", a dva puta veći broj je " + rezultat);
+};
